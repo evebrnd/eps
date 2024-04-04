94ca00ed3b31befb6115639d3b358d1bda254569
ItsNotDaan <513640@student.saxion.nl>
2024-04-04 11:33:55 +0200

    feat: added more text

--- src/app/chapters/two/page.tsx
+++ src/app/chapters/two/page.tsx
@@ -7,9 +7,7 @@ export default function Page() {
         This is the chapyer of Daan
       </h1>
       <p className="leading-7 [&:not(:first-child)]:mt-6">
-        Once upon a time, in a far-off land, there was a very lazy king who
-        spent all day lounging on his throne. One day, his advisors came to him
-        with a problem: the kingdom was running out of money.
+       This is really the chapter of Daan, he 
       </p>
       <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
         The King's Plan
