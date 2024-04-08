d03494f03c946ab949a5ff6033bcd6e6440299af
ItsNotDaan <77987560+ItsNotDaan@users.noreply.github.com>
2024-04-08 12:37:38 +0200

    feat: Change first part of chapter (#12)

--- src/app/chapters/one/page.tsx
+++ src/app/chapters/one/page.tsx
@@ -2,19 +2,19 @@
 
 export default function Page() {
   return (
-    <div className="">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+    <div className="flex flex-col items-center justify-center py-2 mx-4 md:mx-8">
+      <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
         The Joke Tax Chronicles
       </h1>
-      <p className="leading-7 [&:not(:first-child)]:mt-6">
+      <p className="leading-7 mt-6">
         Once upon a time, in a far-off land, there was a very lazy king who
         spent all day lounging on his throne. One day, his advisors came to him
         with a problem: the kingdom was running out of money.
       </p>
-      <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+      <h2 className="mt-10 border-b pb-2 text-3xl font-semibold tracking-tight">
         The King's Plan
       </h2>
-      <p className="leading-7 [&:not(:first-child)]:mt-6">
+      <p className="leading-7 mt-6">
         The king thought long and hard, and finally came up with{" "}
         <a
           href="#"
@@ -31,7 +31,7 @@ export default function Page() {
       <h3 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">
         The Joke Tax
       </h3>
-      <p className="leading-7 [&:not(:first-child)]:mt-6">
+      <p className="leading-7 mt-6">
         The king's subjects were not amused. They grumbled and complained, but
         the king was firm:
       </p>
@@ -40,7 +40,7 @@ export default function Page() {
         <li>2nd level of jokes: 10 gold coins</li>
         <li>3rd level of one-liners : 20 gold coins</li>
       </ul>
-      <p className="leading-7 [&:not(:first-child)]:mt-6">
+      <p className="leading-7 mt-6">
         As a result, people stopped telling jokes, and the kingdom fell into a
         gloom. But there was one person who refused to let the king's
         foolishness get him down: a court jester named Jokester.
@@ -48,13 +48,13 @@ export default function Page() {
       <h3 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">
         Jokester's Revolt
       </h3>
-      <p className="leading-7 [&:not(:first-child)]:mt-6">
+      <p className="leading-7 mt-6">
         Jokester began sneaking into the castle in the middle of the night and
         leaving jokes all over the place: under the king's pillow, in his soup,
         even in the royal toilet. The king was furious, but he couldn't seem to
         stop Jokester.
       </p>
-      <p className="leading-7 [&:not(:first-child)]:mt-6">
+      <p className="leading-7 mt-6">
         And then, one day, the people of the kingdom discovered that the jokes
         left by Jokester were so funny that they couldn't help but laugh. And
         once they started laughing, they couldn't stop.
@@ -62,7 +62,7 @@ export default function Page() {
       <h3 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">
         The People's Rebellion
       </h3>
-      <p className="leading-7 [&:not(:first-child)]:mt-6">
+      <p className="leading-7 mt-6">
         The people of the kingdom, feeling uplifted by the laughter, started to
         tell jokes and puns again, and soon the entire kingdom was in on the
         joke.
@@ -107,12 +107,12 @@ export default function Page() {
           </tbody>
         </table>
       </div>
-      <p className="leading-7 [&:not(:first-child)]:mt-6">
+      <p className="leading-7 mt-6">
         The king, seeing how much happier his subjects were, realized the error
         of his ways and repealed the joke tax. Jokester was declared a hero, and
         the kingdom lived happily ever after.
       </p>
-      <p className="leading-7 [&:not(:first-child)]:mt-6">
+      <p className="leading-7 mt-6">
         The moral of the story is: never underestimate the power of a good laugh
         and always be careful of bad ideas.
       </p>
