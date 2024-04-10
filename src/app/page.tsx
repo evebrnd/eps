306a140bb35dabe375741f8430b3ad12b893d4c5
ItsNotDaan <513640@student.saxion.nl>
2024-04-10 08:57:35 +0200

    feat: added homepage PNG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,50 @@
 'use client'
 import { Button } from "@/components/ui/button";
-import { Link, GraduationCap } from "lucide-react";
+import HomePage from  "../../public/HomePage.png"
+import Image from "next/image"
+import Link from "next/link"
+// import { Link, GraduationCap } from "lucide-react";
 
 export default function Page() {
   return (
-    <div className="bg-primary-foreground px-4">
+    <section>
+      <div className="flex flex-col text-center w-full bg-primary-foreground">
+        <p className="pt-12 italic text-2xl sm:text-5xl">
+          Education of
+        </p>
+        <p className="pt-3 font-extrabold text-4xl sm:text-7xl">
+          Smart Home Devices
+        </p>
+      </div>
+      <div className="w-full my-16 lg:grid lg:grid-cols-2 gap-4">
+        <div className="border-4 flex flex-col items-center justify-center py-12 px-12">
+          <p className="leading-7 mt-6 text-lg sm:text-2xl">
+            The <strong>RELINK project</strong> is a research and documentation project against the unapproachable <strong>transparency</strong> in the <strong>smart home devices</strong> industry.
+          </p>
+          <p className="leading-7 mt-3 text-xl sm:text-2xl">
+            Transparency information about smart home devices is <strong>NOT</strong> accessible to the public.
+          </p>
+          <p className="leading-7 mt-3 text-xl sm:text-2xl">
+            To create more <strong>awareness</strong> and create an easily understandable document the RELINK project is determined to <strong>gather</strong> information, <strong>process</strong> the information and <strong>create</strong> a clear and easily accessible <strong>documentation</strong> format.
+          </p>
+        </div>
+        <div className="border-4 hidden bg-muted lg:block">
+        <Image
+            src={HomePage}
+            alt="Image"
+            width="1920"
+            height="1080"
+            className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+
+
+{/* <div className="bg-primary-foreground px-4">
       <div className="pt-16 pb-8">
         <p className="text-4xl sm:text-6xl italic">
           Education of
@@ -30,6 +70,4 @@ export default function Page() {
           Start learning
         </Button>
       </div>
-    </div>
-  );
-}
+    </div> */}
