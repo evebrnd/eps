f5b4ea4a71532059e6566dbaa99e4852f27a20a6
ItsNotDaan <77987560+ItsNotDaan@users.noreply.github.com>
2024-04-16 15:52:58 +0200

    fix: href mapping was wrong (#26)

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     output: "export",
     basePath: "/eps",
-    assetPrefix: "/eps/",
+    assetPrefix: "/eps",
     reactStrictMode: true,
     images: { unoptimized: true }
 };
