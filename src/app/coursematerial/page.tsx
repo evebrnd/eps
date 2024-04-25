6f5558402f6c24cd46e1b1cb45f5b6a9d4fdda3d
evebrnd <115796475+evebrnd@users.noreply.github.com>
2024-04-25 14:58:50 +0200

    fix(courseMaterial): layout and navbar touches (#52)

--- src/app/coursematerial/page.tsx
+++ src/app/coursematerial/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 
 import Icon from "@mdi/react";
-import { mdiHumanMaleBoardPoll } from "@mdi/js";
 import Sidebar from "@/components/SideBar";
-import PreviousAndNextButton from "@/components/PreviousAndNextButton";
+import { mdiHumanMaleBoardPoll } from "@mdi/js";
+import Link from "next/link"
 import { components_chapters } from "@/utils/constants";
-
+import PreviousAndNextButton from "@/components/PreviousAndNextButton";
 
 export default function Page() {
 
@@ -52,7 +52,7 @@ export default function Page() {
         <h1 id="Introduction" className="flex justify-center text-center mt-6 text-4xl font-extrabold tracking-tight lg:text-5xl">
           Course Material
         </h1>
-        <p className="leading-7 mt-6 text-justify">
+        <p className="leading-7 mt-6">
           We know that the course material is the most important part of the course.
           <br />
           It should be easy to understand and should be well organized. That way it is easy to implement for you, the organizer.
@@ -61,7 +61,7 @@ export default function Page() {
         </p>
 
         {/* Flow of working */}
-        <div className="flex flex-col text-justify py-3 mt-4">
+        <div className="flex flex-col py-3 mt-4">
           <h2 id="flowOfWorking" className="flex justify-normal border-b pb-2 text-3xl font-semibold tracking-tight ">
             Flow of Working
           </h2>
@@ -74,14 +74,14 @@ export default function Page() {
           {/* 5. Lesson part four: The teacher will give the last homework exercise and reflect on the lesson. You have around 10 minutes for this. */}
           {/* 6. Last homework exercise: Using the first written down devices and the risk analasys page on the website the student will determine the risks about the devices and create a small report about it. The teacher can create a own version of this. */}
 
-          <p className="leading-7 mt-2 text-justify">
+          <p className="leading-7 mt-2">
             The flow of working is a step by step plan to give homework exercises, give the lesson and play games.
           </p>
           <div className="">
             <h2 id="homeworkExerciseOne" className="flex justify-normal border-b pb-2 text-2xl font-semibold tracking-tight mt-6">
               1. Homework exercise one
             </h2>
-            <p className="leading-7 mt-2 text-justify">
+            <p className="leading-7 mt-2">
               The first step is to give the students a homework exercise. This exercise is designed to prepare the students for the lesson.
               <br />
               The student will search for three devices inside his/hers house and write down the name of the device and the function of the device.
@@ -92,35 +92,35 @@ export default function Page() {
             <h2 id="lessonPartOne" className="flex justify-normal border-b pb-2 text-2xl font-semibold tracking-tight mt-6">
               2. Lesson part one
             </h2>
-            <p className="leading-7 mt-2 text-justify">
+            <p className="leading-7 mt-2">
               The teacher will explain a bit about the topic using the powerpoint. The teacher has around 10 minutes to do this.
             </p>
 
             <h2 id="lessonPartTwo" className="flex justify-normal border-b pb-2 text-2xl font-semibold tracking-tight mt-6">
               3. Lesson part two
             </h2>
-            <p className="leading-7 mt-2 text-justify">
+            <p className="leading-7 mt-2">
               The teacher will let the students go through the chapters on the website and do little quizzes using the powerpoint in between. The students have around 40 minutes to do this.
             </p>
 
             <h2 id="lessonPartThree" className="flex justify-normal border-b pb-2 text-2xl font-semibold tracking-tight mt-6">
               4. Lesson part three
             </h2>
-            <p className="leading-7 mt-2 text-justify">
+            <p className="leading-7 mt-2">
               The teacher will create groups of four and use the two games and split them between the students. The students have around 30 minutes to do this.
             </p>
 
             <h2 id="lessonPartFour" className="flex justify-normal border-b pb-2 text-2xl font-semibold tracking-tight mt-6">
               5. Lesson part four
             </h2>
-            <p className="leading-7 mt-2 text-justify">
+            <p className="leading-7 mt-2">
               The teacher will give the last homework exercise and reflect on the lesson. The students have around 10 minutes for this.
             </p>
 
             <h2 id="lastHomeworkExercise" className="flex justify-normal border-b pb-2 text-2xl font-semibold tracking-tight mt-6">
               6. Last homework exercise
             </h2>
-            <p className="leading-7 mt-2 text-justify">
+            <p className="leading-7 mt-2">
               The last homework exercise is to determine the risks of the devices the student wrote down in the first homework exercise.
               <br />
               The student will use the risk analysis page on the website to determine the risks of the devices and create a small report about it.
@@ -131,7 +131,7 @@ export default function Page() {
         </div>
 
         {/* Homework exersise */}
-        <div className="flex flex-col text-justify py-3 mt-4">
+        <div className="flex flex-col py-3 mt-4">
           <h2 id="homework" className="flex justify-normal border-b pb-2 text-3xl font-semibold tracking-tight ">
             Homework Exercise
           </h2>
@@ -140,7 +140,7 @@ export default function Page() {
             <h2 id="firstHomeworkExercise" className="flex justify-normal border-b pb-2 text-2xl font-semibold tracking-tight mt-6">
               1. Homework exercise one
             </h2>
-            <p className="leading-7 mt-2 text-justify">
+            <p className="leading-7 mt-2">
               The first homework exercise is to search for three devices inside your house and write down the name of the device and the function of the device.
               <br />
               You will take this list to the lesson.
@@ -149,7 +149,7 @@ export default function Page() {
             <h2 id="lastHomeworkExercise" className="flex justify-normal border-b pb-2 text-2xl font-semibold tracking-tight mt-6">
               2. Last homework exercise
             </h2>
-            <p className="leading-7 mt-2 text-justify">
+            <p className="leading-7 mt-2">
               The last homework exercise is to determine the risks of the devices you wrote down in the first homework exercise.
               <br />
               You will use the risk analysis page on the website to determine the risks of the devices and create a small report about it.
@@ -160,26 +160,26 @@ export default function Page() {
         </div>
 
         {/* Powerpoint */}
-        <div className="flex flex-col text-justify py-3 mt-4">
+        <div className="flex flex-col py-3 mt-4">
           <h2 id="powerpoint" className="flex justify-normal border-b pb-2 text-3xl font-semibold tracking-tight ">
             Powerpoint
           </h2>
           {/* A link to the mentimeter and/or powerpoint should be shown here with a quick explaination */}
-          <p className="leading-7 mt-2 text-justify">
+          <p className="leading-7 mt-2">
             The powerpoint is used to explain the topic to the students. It is also used to give little quizzes in between the chapters.
           </p>
-          <div className="flex justify-center mt-4">
-            <a href="/pdf/powerpoint.pdf" target="_blank" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <div className="flex mt-4 mx-auto text-center">
+            <Link href="/pdf/powerpoint.pdf" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
               Download Powerpoint
-            </a>
-            <a href="https://www.mentimeter.com" target="_blank" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4">
+            </Link>
+            <Link href="https://www.mentimeter.com" target="_blank" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4">
               Go to Mentimeter
-            </a>
+            </Link>
           </div>
         </div>
 
         {/* Games */}
-        <div className="flex flex-col text-justify py-3 mt-4">
+        <div className="flex flex-col py-3 mt-4">
           <h2 id="games" className="flex justify-normal border-b pb-2 text-3xl font-semibold tracking-tight ">
             Games
           </h2>
@@ -188,25 +188,25 @@ export default function Page() {
             <h2 id="gameOne" className="flex justify-normal border-b pb-2 text-2xl font-semibold tracking-tight mt-6">
               1. Game one
             </h2>
-            <p className="leading-7 mt-2 text-justify">
+            <p className="leading-7 mt-2">
               Explain the first game here.
             </p>
             <div className="flex justify-center mt-4">
-              <a href="/pdf/game_one.pdf" target="_blank" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              <Link href="/pdf/game_one.pdf" target="_blank" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 Download Game One
-              </a>
+              </Link>
             </div>
 
             <h2 id="gameTwo" className="flex justify-normal border-b pb-2 text-2xl font-semibold tracking-tight mt-6">
               2. Game two
             </h2>
-            <p className="leading-7 mt-2 text-justify">
+            <p className="leading-7 mt-2">
               Explain the second game here.
             </p>
             <div className="flex justify-center mt-4">
-              <a href="/pdf/game_two.pdf" target="_blank" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              <Link href="/pdf/game_two.pdf" target="_blank" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 Download Game Two
-              </a>
+              </Link>
             </div>
           </div>
         </div>
