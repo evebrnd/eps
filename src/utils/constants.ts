78cab2db07a48a7913af6386d099d62224be83b4
ItsNotDaan <77987560+ItsNotDaan@users.noreply.github.com>
2024-04-26 17:54:16 +0200

    feat: added the AI chapter (#54)
    * feat: added the AI chapter
    feat: changed the text in the insides chapter.
    feat: updated the navigation bar chapters.
    
    * fix: added chapter numbers

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -21,30 +21,49 @@ export const components_about: { title: string; href: string; description: strin
 
 export const components_chapters: { title: string; href: string; description: string }[] = [
     {
-        title: "1. Introduction",
+        // 0
+        title: "1. Introduction", 
         href: "/chapters/one",
         description:
             "What is a smart home device?",
     },
     {
+        // 1
         title: "2. Opinions and studies",
         href: "/chapters/two",
         description:
             "Thoughts of the general public on smart home devices.",
     },
     {
+        // 2
         title: "3. Transparency",
         href: "/chapters/three",
         description:
             "What it means and why it is important.",
     },
     {
+        // 3
         title: "4. Internal workings",
         href: "/chapters/four",
         description:
             "Internals working of smart home devices.",
     },
     {
+        // 4
+        title: "5. Artificial Intelligence",
+        href: "/chapters/five",
+        description:
+            "The role of AI in smart home devices.",
+    },
+    {
+        // 5
+        title: "6. Usabilities and Dangers",
+        href: "/chapters/six",
+        description:
+            "The usabilities and dangers of smart home devices.",
+    },
+    {
+        // 6
         title: "Resources",
         href: "/chapters/resources",
         description:
