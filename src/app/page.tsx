1164dfffe6c7c5b91209585dd630416457393746
ItsNotDaan <513640@student.saxion.nl>
2024-04-29 14:21:48 +0200

    feat: first check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,8 @@ import Image from "next/image"
 import Link from "next/link"
 import { GraduationCap } from "lucide-react";
 
+import Icon from "@mdi/react";
+
 import {
   Carousel,
   CarouselContent,
@@ -36,8 +38,8 @@ export default function Page() {
             <div className="flex flex-row justify-center lg:justify-normal">
               <Button asChild size="rounded" className="text-lg mt-14 sm:text-2xl bg-primary/80">
                 <Link href={components_chapters[0].href} replace>
-                <GraduationCap className="w-8 h-8 mr-4" />
-                Start learning
+                  <GraduationCap className="w-8 h-8 mr-4" />
+                  Start learning
                 </Link>
               </Button>
             </div>
@@ -57,14 +59,28 @@ export default function Page() {
         <Carousel className="">
           <CarouselContent className="-ml-1">
             {components_chapters.map((component, index) => (
-              <CarouselItem key={index} className="flex flex-row justify-center pl-1 md:basis-1/2 lg:basis-1/4">
+              <CarouselItem key={index} className="flex flex-row pl-1 md:basis-1/2 lg:basis-1/4">
+
                 <div className="p-1 text-center">
-                  <Card
-                    title={component.title}
-                    caption={component.description}
-                    href={component.href}
-                  />
+                  <div className="max-w-sm bg-white border border-gray-200 rounded-lg overflow-hidden shadow hover:shadow-lg active:shadow-lg">
+                    <Link href={component.href}>
+                      <Icon path={component.icon} size="4em" className="mx-auto mt-7" />
+                    </Link>
+                    <div className="p-5">
+                      <Link href={component.href}>
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 hover:underline hover:decoration-wavy hover:decoration-primary">{component.title}</h5>
+                      </Link>
+                      <p className="mb-3 font-normal text-gray-700">{component.description}</p>
+                      <Link href={component.href} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-destructive rounded-lg hover:shadow-lg focus:ring-4 focus:outline-none focus:ring-blue-300">
+                        Read more
+                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                        </svg>
+                      </Link>
+                    </div>
+                  </div>
                 </div>
+
               </CarouselItem>
             ))}
           </CarouselContent>
