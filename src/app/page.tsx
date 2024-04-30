9be7734ff1c9c7ceaa2bb11968511e9d63e1356a
ItsNotDaan <77987560+ItsNotDaan@users.noreply.github.com>
2024-04-30 18:38:49 +0200

    59 change the design of the homescreen + name (#61)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 import { Button } from "@/components/ui/button";
-import Card from "@/components/Card"
 import { components_chapters } from "@/utils/constants";
 import HomePage from "../../public/HomePage.png"
+import HomePage_Mobile from "../../public/HomePage_Mobile.png"
 import Image from "next/image"
 import Link from "next/link"
 import { GraduationCap } from "lucide-react";
+
+import Icon from "@mdi/react";
 
 import {
   Carousel,
@@ -18,8 +20,8 @@ import {
 export default function Page() {
   return (
     <section>
-      <div className="w-full h-vh bg-primary/10 md:grid lg:grid-cols-8 md:px-12">
-        <div className="flex flex-col justify-center lg:col-span-3 px-4 py-6 md:py:12">
+      <div className="w-full lg:h-svh sm:items-center bg-primary/10 grid sm:grid-cols-8 md:px-12">
+        <div className="flex flex-col justify-center sm:col-span-5 lg:col-span-3 px-4 py-6 md:py-12">
           <p className="pt-3 text-3xl italic md:pt-12 sm:text-justify">
             Education on
           </p>
@@ -33,11 +35,11 @@ export default function Page() {
             <p className="mt-6 text-xl leading-7">
               To create more awareness and have an easier way of explaining this, the RELINK project developed a course for you.
             </p>
-            <div className="flex flex-row justify-center lg:justify-normal">
+            <div className="flex flex-row justify-center md:justify-normal">
               <Button asChild size="rounded" className="text-lg mt-14 sm:text-2xl bg-primary/80">
                 <Link href={components_chapters[0].href} replace>
-                <GraduationCap className="w-8 h-8 mr-4" />
-                Start learning
+                  <GraduationCap className="w-8 h-8 mr-4" />
+                  Start learning
                 </Link>
               </Button>
             </div>
@@ -49,21 +51,47 @@ export default function Page() {
             alt="Image"
             width="1920"
             height="1080"
-            className="h-[90vh] w-full object-scale-down"
+            className="lg:h-[90vh] w-full object-scale-down"
+          />
+        </div>
+        <div className="hidden sm:col-span-3 sm:block lg:hidden">
+          <Image
+            src={HomePage_Mobile}
+            alt="Image"
+            width={1000}
+            height={2000}
+            className="sm:h-[90vh] w-full object-cover"
           />
         </div>
       </div>
-      <div className="flex flex-col w-full px-12 pt-12 md:px-24">
+
+      <p className="pt-6 text-4xl font-extrabold text-center">
+            Course Chapters
+      </p>
+
+      <div className="flex flex-col w-full px-12 pt-6 md:px-24">
         <Carousel className="">
-          <CarouselContent className="-ml-1">
+          <CarouselContent className="md:-ml-8 items-center">
             {components_chapters.map((component, index) => (
-              <CarouselItem key={index} className="flex flex-row justify-center pl-1 md:basis-1/2 lg:basis-1/4">
+              <CarouselItem key={index} className="md:pl-8 md:basis-1/2 lg:basis-1/4 max-w-80">
                 <div className="p-1 text-center">
-                  <Card
-                    title={component.title}
-                    caption={component.description}
-                    href={component.href}
-                  />
+                  <div className="max-w-sm bg-white border border-gray-200 rounded-lg overflow-hidden shadow hover:shadow-lg active:shadow-lg">
+                    <Link href={component.href}>
+                      <Icon path={component.icon} size="4em" className="mx-auto mt-2" />
+                    </Link>
+                    <div className="p-5">
+                      <Link href={component.href}>
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 hover:underline hover:decoration-wavy hover:decoration-primary">{component.title}</h5>
+                      </Link>
+                      <p className="mb-3 font-normal text-gray-700">{component.description}</p>
+                      <Link href={component.href} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-destructive rounded-lg hover:shadow-lg focus:ring-4 focus:outline-none focus:ring-blue-300">
+                        Read more
+                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                        </svg>
+                      </Link>
+                    </div>
+                  </div>
                 </div>
               </CarouselItem>
             ))}
@@ -72,6 +100,7 @@ export default function Page() {
           <CarouselNext />
         </Carousel>
       </div>
+
     </section>
   );
 }
